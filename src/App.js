d475eb1e8e33d0375a37658f530c94a2613e9791
David Doan <daviddoan@davids-mbp-6.devices.brown.edu>
2022-11-10 13:20:54 -0500

    functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -12,17 +13,73 @@ function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
+  const initialState = []
+
+  const [variantInfo, setVariantInfo] = useState(initialState)
+  const [total, setTotal] = useState(0.00)
+
+  function handleSize(item) {
+    setVariantInfo(variantInfo => [...variantInfo, item]);
+  }
+
+  function handleTotal(item) {
+    setTotal(Math.round((total+item + Number.EPSILON) * 100) / 100);
+  }
+	
+  function myFunction(event, name, price){
+    console.log(variantInfo)
+    handleSize(name);
+    handleTotal(price);
+  }
+
+  function MyButton({ item, onClick }) {
+    // console.log(item.price)
+    return (
+      <button onClick={event => myFunction(event, item.name, item.price)}>
+        <p>Add to Cart</p>
+      </button>
+    );
+  }
+
+  let content;
+  if (variantInfo?.length > 0) {
+    let visited = []
+    let movies = 0
+    content = variantInfo.map(item => {
+          if (!visited.includes(item)){
+            movies = variantInfo.filter(item1 => item1 === item)
+            visited = [...visited, item]
+            return <p>{item} x{movies.length}</p>
+          }
+
+    })
+  } else {
+    content = <p>Nothing in your cart... Yet!</p>
+  }
+
+  console.log(content)
+
+
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>The Bakery of Grinds and Sighs</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <div>
+          <BakeryItem item={item}></BakeryItem>
+          <MyButton item={item} onClick={myFunction}/>
+        </div>
       ))}
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <div>
+          <p>${total}</p>
+          <div>
+          {content}
+          </div>
+        </div>
+            
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,17 @@
 // TODO: create a component that displays a single bakery item
+export default function BakeryItem({item}){
+    return (
+        <div>
+    <h2>{item.name}</h2>
+    <p>{item.description}</p>
+    <h3>{item.price}</h3>
+    <img
+        src={item.image}
+        style={{
+        width: 100,
+        height: 100
+        }}
+    />
+</div>
+    )
+}
